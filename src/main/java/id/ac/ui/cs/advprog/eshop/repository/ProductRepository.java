834de92018a0d667c0ae07809fecd20ac760139b
AthallahD <athallah2209@gmail.com>
2025-02-28 18:46:57 +0700

    fix : edit nya ketinggalan

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -7,5 +7,6 @@ public interface ProductRepository extends Create<Product>, Find<Product> {
     Product create(Product product);
     Product findById(String productId);
     Product delete(String productId);
+    Product edit(Product product, String productId);
     Iterator<Product> findAll();
 }
