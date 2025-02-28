08848e326295c897b2bb44fb897109ac3ae43e01
AthallahD <athallah2209@gmail.com>
2025-02-28 18:39:11 +0700

    feat : implement ISP (ketinggalan)

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -2,7 +2,7 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import java.util.Iterator;
 
-public interface CarRepository {
+public interface CarRepository extends Create<Car>, Delete, Find<Car>, Update<Car> {
     Car create(Car car);
     Iterator<Car> findAll();
     Car findById(String id);
--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.Iterator;
 
-public interface ProductRepository {
+public interface ProductRepository extends Create<Product>, Find<Product> {
     Product create(Product product);
     Product findById(String productId);
     Product delete(String productId);
