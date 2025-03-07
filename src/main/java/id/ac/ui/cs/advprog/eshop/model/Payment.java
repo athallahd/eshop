88794b8d309c190e320c36f793957c53c6acbd8d
AthallahD <athallah2209@gmail.com>
2025-03-07 18:02:28 +0700

    [GREEN] Implement Payment Model (correct version)

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Builder
 @Getter
@@ -19,10 +20,58 @@ public class Payment {
     @Setter
     private String status;
 
-    public Payment(String id, Order order, String method, Map<String, String> PaymentData){
-
+    public Payment(String id, Order order, String method, Map<String, String> paymentData){
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.setStatusPaymentData(paymentData, method);
+        this.paymentData = paymentData;
     }
-    public Payment(String id, Order order, String method, Map<String, String> PaymentData, String status){
+    public Payment(String id, Order order, String method, Map<String, String> paymentData, String status){
+        this(id, order, method, paymentData);
 
+        String[] statusList = {"PENDING","SUCCESS", "REJECTED"};
+        if(Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))){
+            throw new IllegalArgumentException();
+        }else{
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status){
+        String[] statusList = {"PENDING","SUCCESS", "REJECTED"};
+        if(Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))){
+            throw new IllegalArgumentException();
+        }else{
+            this.status = status;
+        }
+    }
+
+    public void setStatusPaymentData(Map<String, String> paymentData, String paymentMethod) {
+        if (paymentMethod.equals("BANK")) {
+            if (paymentData.get("bankName") == null || paymentData.get("referenceCode") == null || Objects.equals(paymentData.get("bankName"), "")
+                    || Objects.equals(paymentData.get("referenceCode"), "")) {
+                this.status = "REJECTED";
+            }else{
+                this.status = "PENDING";
+            }
+        }else if (paymentMethod.equals("VOUCHER_CODE")) {
+            String voucherCode = paymentData.get("voucherCode");
+
+            if (voucherCode == null || voucherCode.length() != 16 || !voucherCode.startsWith("ESHOP")) {
+                this.status = "REJECTED";
+                return;
+            }
+
+            long digitCount = voucherCode.chars()
+                    .filter(Character::isDigit)
+                    .count();
+
+            if (digitCount != 8) {
+                this.status = "REJECTED";
+            } else {
+                this.status = "SUCCESS";
+            }
+        }
     }
 }
