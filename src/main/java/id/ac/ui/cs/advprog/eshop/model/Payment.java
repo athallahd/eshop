356d9b53c5eddde3c68dd062083768515f3c409d
AthallahD <athallah2209@gmail.com>
2025-03-07 17:14:41 +0700

    [RED] Add skeleton for Payment model (correct version)

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,32 +1,28 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
+@Builder
 @Getter
-@Setter
 public class Payment {
     private String id;
-    private String method;
-    private String status;
-    private Map<String, String> paymentData;
     private Order order;
+    private String method;
+    private Map<String,String> paymentData;
 
-    public Payment(String id, Order order, String method, String status, Map<String, String> paymentData) {
-        if (order == null) {
-            throw new IllegalArgumentException("Order cannot be null");
-        }
-        if (paymentData == null) {
-            throw new IllegalArgumentException("Payment data cannot be null");
-        }
+    @Setter
+    private String status;
 
-        this.id = (id != null && !id.isEmpty()) ? id : UUID.randomUUID().toString();
-        this.order = order;
-        this.paymentData = paymentData;
-        this.method = method;
-        this.status = "WAITING_PAYMENT";
+    public Payment(String id, Order order, String method, Map<String, String> PaymentData){
+
+    }
+    public Payment(String id, Order order, String method, Map<String, String> PaymentData, String status){
+
     }
 }
